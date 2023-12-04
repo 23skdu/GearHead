0abe8bf514c895b3fdf5ace58ca165ec123abb2e
23skdu <23skdu@users.noreply.github.com>
2023-12-03 21:30:59 -0800

    latest

--- gearhead/src/main.rs
+++ gearhead/src/main.rs
@@ -1,47 +1,20 @@
-use std::net::Ipv4Addr;
 use sqlite::State;
 use axum::{
-    extract::{FromRef, Path},
-    response::IntoResponse,
-    routing::get,
-    serve, Router,
+    routing::{get, post},
+    http::StatusCode,
+    Json, Router,
 };
 use axum_template::{engine::Engine, Key, RenderHtml};
 use minijinja::Environment;
-use serde::Serialize;
-use tokio::net::TcpListener;
-type AppEngine = Engine<Environment<'static>>;
-#[derive(Debug, Serialize)]
-pub struct Person {
-    name: String,
-}
-async fn get_name(
-    engine: AppEngine,
-    Key(key): Key,
-    Path(name): Path<String>,
-) -> impl IntoResponse {
-    let person = Person { name };
-    RenderHtml(key, engine, person)
-}
-#[derive(Clone, FromRef)]
-struct AppState {
-    engine: AppEngine,
-}
+
 #[tokio::main]
 async fn main() {
-    let mut jinja = Environment::new();
-    jinja
-        .add_template("/:name", "<h1>Hello</h1><p>{{name}}</p>")
-        .unwrap();
-    let app = Router::new()
-        .route("/:name", get(get_name))
-        // Create the application state
-        .with_state(AppState {
-            engine: Engine::from(jinja),
-        });
+	tracing_subscriber::fmt::init();
+	let app = Router::new()
+        .route("/", get(root))
+        .route("/metrics", get(metrics));
 
-    let listener = TcpListener::bind((Ipv4Addr::LOCALHOST, 1118))
-        .await
-        .unwrap();
-    serve(listener, app.into_make_service()).await.unwrap();
+
+
+
 }
